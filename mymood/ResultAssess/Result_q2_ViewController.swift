6ad2022a30b80919a664c99fc0d797c33f4a19a0
stamp <stampc07@gmail.com>
2020-10-27 00:47:44 +0700

    as to resultas

--- mymood/ResultAssess/Result_q2_ViewController.swift
+++ mymood/ResultAssess/Result_q2_ViewController.swift
@@ -96,7 +96,8 @@ class Result_q2_ViewController: UIViewController {
                     let alert = UIAlertController(title: "คุณไม่มีแนวโน้มที่จะเป็นโรคซึมเศร้า", message: nil, preferredStyle: .alert)
                     alert.addAction(UIAlertAction(title: "ตกลง", style: .cancel, handler:{(action) -> Void in
                         self.navigationController!.popViewController(animated: true)
-                        
+                        let vc = self.storyboard?.instantiateViewController(withIdentifier: "MyTabBarController") as! TabBarController
+                        self.present(vc, animated: true, completion: nil)
                     }))
                     self.present(alert, animated: true, completion: nil)
                     
