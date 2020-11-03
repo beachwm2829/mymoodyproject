bfadaf2d9c6614a44cd566c5c9fc4ed91cb6c1e3
khanathipstamp <stampc07@gmail.com>
2020-11-04 03:16:53 +0700

    seach Assement

--- mymood/Search/SearchAssementViewController.swift
+++ mymood/Search/SearchAssementViewController.swift
@@ -1,29 +1,39 @@
 //
-//  Assessment_TableViewController.swift
+//  SearchAssementViewController.swift
 //  mymood
 //
-//  Created by macOS on 10/2/20.
+//  Created by macOS on 11/3/20.
 //  Copyright © 2020 Manasawee Kaenampornpan. All rights reserved.
-//
+//SearchAssementViewController
+
 
 import UIKit
 import Alamofire
 import AlamofireObjectMapper
 import SwiftyJSON
 
-class Assessment_TableViewController: UITableViewController {
+class SearchAssementViewController: UIViewController,UITableViewDelegate,UITableViewDataSource, UIPickerViewDelegate,UIPickerViewDataSource{
     
     struct AssessmentModel :Decodable {
            let nameAs :String
            let timeAs :String
            let dateAs :String
            let resultAs :String
+        let mounth :String
+        let year :String
        }
-    var AsArray = ["Q2":"แบบคัดกรองโรคซึมเศร้าด้วย 2 คำถาม (2Q)","Q9":"แบบประเมินโรคซึมเศร้าด้วย 9 คำถาม ( 9Q)","Q8":"แบบประเมินการฆ่าตัวตาย (8Q)"]
+
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var datemonthPicker: UIPickerView!
     
+    var AsArray = ["Q2":"แบบคัดกรองโรคซึมเศร้าด้วย 2 คำถาม (2Q)","Q9":"แบบประเมินโรคซึมเศร้าด้วย 9 คำถาม ( 9Q)","Q8":"แบบประเมินการฆ่าตัวตาย (8Q)"]
+    var months = ["All","Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
+    var year = [String]()
+    var yearArr = [String]()
     var asId: String?
     var AssData = [AssessmentModel]()
     var AsData = [String]()
+    var Years = [String]()
     
     var AsessmentArray = [AssessmentModel]()
     var CurrentAsesArray = [AssessmentModel]()
@@ -32,23 +42,77 @@ class Assessment_TableViewController: UITableViewController {
         super.viewDidLoad()
         print("ASSemment**")
         print(asId)
+//        dateTimePicker.heightA
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        AssData.removeAll()
+        yearArr.removeAll()
+        self.getAssessment()
+       
+        datemonthPicker.delegate = self
+        datemonthPicker.dataSource = self
+        
     }
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 2
     }
-    override func viewWillAppear(_ animated: Bool) {
-        AssData.removeAll()
-        self.getAssessment()
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if component == 0{
+            return months.count
+        }
+        return Years.count
     }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if component == 0{
+            return months[row]
+        }
+        return Years[row]
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if (component == 0){
+            if(months[row] == "All"){
+                CurrentAsesArray = AssData
+                self.tableView.reloadData()
+            }else{
+                search(row: row,component:component)
+            }
+        }else{
+            if(Years[row] == "All"){
+                CurrentAsesArray = AssData
+                self.tableView.reloadData()
+            }else{
+                search(row: row,component:component)
+            }
+        }
+    }
+    func search(row: Int,component:Int){
+        print(row)
+        if(component == 0){
+                    CurrentAsesArray = AssData.filter({ AssessmentModel -> Bool in
+            //            AssessmentModel.year == Years[row] ||
+                            AssessmentModel.mounth == months[row]
+                    })
+                    self.tableView.reloadData()
+        }else{
+            print("Years\(self.Years)")
+            CurrentAsesArray = AssData.filter({ AssessmentModel -> Bool in
+    //            AssessmentModel.year == Years[row] ||
+                    AssessmentModel.year == Years[row]
+            })
+            self.tableView.reloadData()
+        }
+    }
+
     // MARK: - Table view data source
     
     @IBAction func toQ2As(_ sender: Any) {
         self.performSegue(withIdentifier: "toAses", sender: self)
     }
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "AssessmentCell", for: indexPath) as? Assessment_TableViewCell
-        let index = AssData[indexPath.row]
-        print("AssData ----->\(AsArray)")
+        let index = CurrentAsesArray[indexPath.row]
+        print("AssData ----->\(CurrentAsesArray)")
         cell?.nameAs.text = index.nameAs
         cell?.resultAs.text = index.resultAs
         cell?.timeAs.text = index.timeAs
@@ -79,22 +143,35 @@ class Assessment_TableViewController: UITableViewController {
                      let resultAs = aAssessment["result"].stringValue
                      let dateAs = aAssessment["date"].stringValue
                      let timeAs = aAssessment["time"].stringValue
-                   
-                    let AsData = AssessmentModel(nameAs: nameAs, timeAs: timeAs, dateAs: dateAs, resultAs: resultAs)
+                    var years = ""
+                    let array = dateAs.components(separatedBy: "-")
+                    self.yearArr.append(array[0])
+                    years = array[0]
+                    
+                    
+                    let arraym = dateAs.components(separatedBy: "-")
+                    var mounths = arraym[1]
+                    if(mounths == "01"){mounths = "Jan"}else if(mounths == "02"){mounths = "Feb"}else if(mounths == "03"){mounths = "Mar"}else if(mounths == "04"){mounths = "Apr"}else if(mounths == "05"){mounths = "May"}else if(mounths == "06"){mounths = "Jun"}else if(mounths == "07"){mounths = "Jul"}else if(mounths == "08"){mounths = "Aug"}else if(mounths == "09"){mounths = "Sep"}else if(mounths == "10"){mounths = "Oct"}else if(mounths == "11"){mounths = "Nov"}else if(mounths == "12"){mounths = "Dec"}
+                    let AsData = AssessmentModel(nameAs: nameAs, timeAs: timeAs, dateAs: dateAs, resultAs: resultAs, mounth: mounths , year: years)
+                    
                      self.AssData.append(AsData)
                     print(" self.AssData\( self.AssData)")
-                    
+                    self.year.append(dateAs)
                     self.CurrentAsesArray = self.AssData
                  }
-                 self.tableView.reloadData()
+                
+                self.tableView.reloadData()
              }catch{
                  print("error")
              }
+           self.yearArr.insert("All", at: 0)
+           self.Years = Array(Set(self.yearArr))
+            self.Years.sort()
+           print("yearArr\(self.yearArr)")
+//            print("Years\(self.Years)")
+            self.datemonthPicker.reloadComponent(1)
          }
      }
-    @IBAction func btSearchAs(_ sender: Any) {
-        self.performSegue(withIdentifier: "tosearchAssem", sender: self)
-    }
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "toAses"{
             if let Result_q2_ViewController = segue.destination as? NavigationController{
@@ -103,20 +180,15 @@ class Assessment_TableViewController: UITableViewController {
                 }
             }
         }
-        if segue.identifier == "tosearchAssem"{
-            let SearchAssementViewController = segue.destination as! SearchAssementViewController
-            print(self.asId)
-            SearchAssementViewController.asId = self.asId
-        }
     }
-    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         
         return 90
     }
 
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return AssData.count
+        return CurrentAsesArray.count
     }
 
     /*
@@ -145,7 +217,7 @@ class Assessment_TableViewController: UITableViewController {
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }
     }
     */
 
---
+++ mymood/Search/searchAssementTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  searchAssementTableViewCell.swift
+//  mymood
+//
+//  Created by macOS on 11/3/20.
+//  Copyright © 2020 Manasawee Kaenampornpan. All rights reserved.
+//
+
+import UIKit
+
+class searchAssementTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
